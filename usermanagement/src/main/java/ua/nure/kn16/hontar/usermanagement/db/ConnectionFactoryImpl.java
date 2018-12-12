27386c91bca4904ef4228e086ee76839b86ec83d
serhii.hontar <serhii.hontar@nure.ua>
2018-12-12 20:52:13 +0200

    change DaoFactory, ConnectionFactoryImpl

--- usermanagement/src/main/java/ua/nure/kn16/hontar/usermanagement/db/ConnectionFactoryImpl.java
+++ usermanagement/src/main/java/ua/nure/kn16/hontar/usermanagement/db/ConnectionFactoryImpl.java
@@ -3,6 +3,7 @@ package ua.nure.kn16.hontar.usermanagement.db;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 
 public class ConnectionFactoryImpl implements ConnectionFactory {
@@ -25,6 +26,13 @@ public class ConnectionFactoryImpl implements ConnectionFactory {
 }
 
 
+
+	public ConnectionFactoryImpl(Properties properties) {
+		user = properties.getProperty("connection.user");
+		password = properties.getProperty("connection.password");
+		url = properties.getProperty("connection.url");
+		driver = properties.getProperty("connection.driver");
+	}
 
 	@Override
 	public Connection createConnection() throws DatabaseException {
